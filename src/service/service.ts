57360a9fbfb55dc859975aa377e26542e286e3ee
José Beita <jo13bc@gmail.com>
2022-11-17 14:48:07 -0600

    Update service.ts

--- src/service/service.ts
+++ src/service/service.ts
@@ -8,44 +8,16 @@ export class Service<E extends Entity> {
     this.entity_name = entity_name;
   }
 
-  async insert(entity: E): Promise<any> {
-    return await fetch(`/.netlify/functions/${this.entity_name}`, {
-      headers: { "Content-Type": "application/json" },
-      method: "POST",
-      mode: "cors",
-      credentials: "same-origin",
-      body: JSON.stringify(entity),
-    }).then((response) => response.json());
-  }
-
-  async update(entity: E): Promise<any> {
-    return await fetch(`/.netlify/functions/${this.entity_name}/${entity._id}`, {
-      headers: { "Content-Type": "application/json" },
-      method: "PUT",
-      mode: "cors",
-      credentials: "same-origin",
-      body: JSON.stringify(entity),
-    }).then((response) => response.json());
-  }
-
-  async delete(_id: ObjectId): Promise<any> {
-    return await fetch(`/.netlify/functions/${this.entity_name}/${_id}`, {
-      headers: { "Content-Type": "application/json" },
-      method: "DELETE",
-      mode: "cors",
-      credentials: "same-origin",
-    }).then((response) => response.json());
-  }
-
-  async find(_id: ObjectId): Promise<E> {
-    return new Promise<E>((resolve, reject) => {
-      fetch(`/.netlify/functions/${this.entity_name}/${_id}`, {
+  async insert(entity: E): Promise<any | undefined> {
+    return new Promise<any | undefined>((resolve, reject) => {
+      fetch(`/.netlify/functions/${this.entity_name}`, {
         headers: { "Content-Type": "application/json" },
-        method: "GET",
+        method: "POST",
         mode: "cors",
         credentials: "same-origin",
+        body: JSON.stringify(entity),
       }).then((response) => response.json())
-        .then((response: Response<E>) => {
+        .then((response: Response<any | undefined>) => {
           if (response.status === 200) {
             resolve(response.body);
           } else {
@@ -57,15 +29,76 @@ export class Service<E extends Entity> {
     });
   }
 
-  list(): Promise<Array<E>> {
-    return new Promise<Array<E>>((resolve, reject) => {
+  async update(entity: E): Promise<any | undefined> {
+    return new Promise<any | undefined>((resolve, reject) => {
+      fetch(`/.netlify/functions/${this.entity_name}/${entity._id}`, {
+        headers: { "Content-Type": "application/json" },
+        method: "PUT",
+        mode: "cors",
+        credentials: "same-origin",
+        body: JSON.stringify(entity),
+      }).then((response) => response.json())
+        .then((response: Response<any | undefined>) => {
+          if (response.status === 200) {
+            resolve(response.body);
+          } else {
+            reject(response);
+          }
+        }).catch(error => {
+          reject(error);
+        });
+    });
+  }
+
+  async delete(_id: ObjectId): Promise<any | undefined> {
+    return new Promise<any | undefined>((resolve, reject) => {
+      fetch(`/.netlify/functions/${this.entity_name}/${_id}`, {
+        headers: { "Content-Type": "application/json" },
+        method: "DELETE",
+        mode: "cors",
+        credentials: "same-origin",
+      }).then((response) => response.json())
+        .then((response: Response<any | undefined>) => {
+          if (response.status === 200) {
+            resolve(response.body);
+          } else {
+            reject(response);
+          }
+        }).catch(error => {
+          reject(error);
+        });
+    });
+  }
+
+  async find(_id: ObjectId): Promise<E | undefined> {
+    return new Promise<E | undefined>((resolve, reject) => {
+      fetch(`/.netlify/functions/${this.entity_name}/${_id}`, {
+        headers: { "Content-Type": "application/json" },
+        method: "GET",
+        mode: "cors",
+        credentials: "same-origin",
+      }).then((response) => response.json())
+        .then((response: Response<E | undefined>) => {
+          if (response.status === 200) {
+            resolve(response.body);
+          } else {
+            reject(response);
+          }
+        }).catch(error => {
+          reject(error);
+        });
+    });
+  }
+
+  list(): Promise<Array<E> | undefined> {
+    return new Promise<Array<E> | undefined>((resolve, reject) => {
       fetch(`/.netlify/functions/${this.entity_name}`, {
         headers: { "Content-Type": "application/json" },
         method: "GET",
         mode: "cors",
         credentials: "same-origin",
       }).then((response) => response.json())
-        .then((response: Response<Array<E>>) => {
+        .then((response: Response<Array<E> | undefined>) => {
           if (response.status === 200) {
             resolve(response.body);
           } else {
